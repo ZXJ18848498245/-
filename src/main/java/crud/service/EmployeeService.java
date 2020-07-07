7e985cabe3ace5e3837dbd6007063209b6c72e4e
zxj <zxj123@163.com>
2020-07-07 15:13:13 +0800

    员工信息管理系统

--- src/main/java/crud/service/EmployeeService.java
+++ src/main/java/crud/service/EmployeeService.java
@@ -12,6 +12,9 @@ import java.util.List;
 @Service
 public class EmployeeService {
 
+    //@Autowired 注释，它可以对类成员变量、方法及构造函数进行标注，完成自动装配的工作。 通过 @Autowired的使用来消除 set ，get方法。
+    //在使用@Autowired之前，我们对一个bean配置起属性时，是这用用的
+    //<property name="属性名" value=" 属性值"/>
     @Autowired
     EmployeeMapper employeeMapper;
     /*
